d20b32f9e917f3729a8305a5113d02c437589016
Dhananjay <patel.iec@gmail.com>
2016-08-28 11:11:11 +0530

    frontend files

---
+++ server.js
@@ -0,0 +1,19 @@
+var PORT = process.env.PORT || 3000;
+
+var express = require("express");
+
+var app = express();
+
+var http = require("http").Server(app);
+
+var io = require("socket.io")(http);
+
+io.on("connection", function(){
+    console.log("Client connected via socket");
+})
+
+app.use(express.static(__dirname + "/public"));
+
+http.listen(PORT, function(){
+    console.log("Server Started ap port:"+PORT)
+})
